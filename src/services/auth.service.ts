4e0e4ac9bb4a676932b80107858c6c2beae786f4
Your Name <jafalkaff96@gmail.com>
2025-12-23 16:22:15 +0700

    create service register

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,10 +1,22 @@
 import { getApi } from "./axiosClient";
-import type { LoginPayload, LoginResponse } from "../types/auth.type";
+import type {
+  LoginPayload,
+  LoginResponse,
+  RegisterPayload,
+  RegisterResponse,
+} from "../types/auth.type";
 
 export const authService = {
   async login(payload: LoginPayload): Promise<LoginResponse> {
     const response = await getApi().post<LoginResponse>("/auth/login", payload);
 
+    return response.data;
+  },
+  async register(payload: RegisterPayload): Promise<RegisterResponse> {
+    const response = await getApi().post<RegisterResponse>(
+      "/auth/register",
+      payload
+    );
     return response.data;
   },
 };
