668d967d6ac0dd5890ca6b0aa36e3a3c0f67ede3
Your Name <jafalkaff96@gmail.com>
2025-12-29 12:04:30 +0700

    add task schema

--- src/validations/main.ts
+++ src/validations/main.ts
@@ -1,6 +1,8 @@
 import { loginSchema, registerSchema } from "./auth.schema";
+import { taskSchema } from "./task.schema";
 
 export const validations = {
   login: loginSchema,
   register: registerSchema,
+  task: taskSchema,
 };
